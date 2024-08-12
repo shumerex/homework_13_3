034270ac51c6310bd2fb2dfbdc00cbc5b121a5c5
shumerex <163420582+shumerex@users.noreply.github.com>
2024-08-12 22:00:23 +0700

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = ""
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+    print('start')
+
+@dp.message_handler()
+async def all_messages(message: types.Message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+    print('all_messages')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
